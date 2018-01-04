a2d1d0128e5da5375afb36d1da61545772c4db59
shengjian <531522023@qq.com>
2018-01-04 11:12:45 +0800

    加入详细日志查看奖励发放情况

--- kk-nationalPK-server/src/main/java/com/melot/kk/nationalPK/server/impl/ResActorLadderMatchServiceImpl.java
+++ kk-nationalPK-server/src/main/java/com/melot/kk/nationalPK/server/impl/ResActorLadderMatchServiceImpl.java
@@ -1,6 +1,7 @@
 package com.melot.kk.nationalPK.server.impl;
 
 import com.google.common.collect.Lists;
+import com.google.gson.Gson;
 import com.melot.common.driver.service.MessageService;
 import com.melot.kk.nationalPK.api.constant.GameDanEnum;
 import com.melot.kk.nationalPK.api.constant.GiveRewardStatusEnum;
@@ -184,6 +185,7 @@ public class ResActorLadderMatchServiceImpl implements ResActorLadderMatchServic
 
                     int offset = pageIndex * limit;
                     List<ResActorLadderMatch> resActorLadderMatches = resActorLadderMatchMapper.getList(seasonId, limit, offset);
+                    logger.error("正在发放奖励的主播列表 " + new Gson().toJson(resActorLadderMatches));
                     if(resActorLadderMatches == null || resActorLadderMatches.size() == 0) {
                         break;
                     }
